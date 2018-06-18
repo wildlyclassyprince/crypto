e1ecd38b3a9ee0e7c95eaba072769d2963c91612
Mthuli Bhebhe <wildlyclassyprince@users.noreply.github.com>
2018-06-18 16:58:53 +0200

    Added comments

--- tiny_blockchain/transact.sh
+++ tiny_blockchain/transact.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/bash --
+
+# To initiate a transaction over the blockchain
 curl "localhost:5000/txion" \
 			-H "Content-Type: application/json" \
 			-d '{"from": "Sender 1", "to":"Receiver 1", "amount": 5}'
