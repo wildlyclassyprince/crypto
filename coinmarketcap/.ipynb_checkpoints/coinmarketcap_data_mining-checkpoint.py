828818dd5703ad3ab12cb6ba6d4fdaf2b4d95b42
wildlyclassyprince <lihtumb@gmail.com>
2018-08-20 15:55:55 +0200

    Updated notebook and script

--- coinmarketcap/.ipynb_checkpoints/coinmarketcap_data_mining-checkpoint.py
+++ coinmarketcap/.ipynb_checkpoints/coinmarketcap_data_mining-checkpoint.py
@@ -28,8 +28,11 @@ df = pd.read_html(url, attrs = {'id': 'assets-all'})[0]
 # New column names (there is a new column at the end of the column list):
 df.columns = ['#', 'Name', 'Platform', 'MarketCap', 'Price', 'Supply', 'VolumeDay', 'pctHour', 'pctDay', 'pctWeek', 'NewCol']
 
+# Dropping the new column
+df = df.drop('NewCol', axis=1)
+
 # Build an upper case name column so we can sort on it more easily
-df['NameUpper'] = map(lambda x: x.upper(), df['Name'])
+#df['NameUpper'] = map(lambda x: x.upper(), df['Name'])
 
 # Cleaning numeric data:
 df['Price'] = df['Price'].str.replace('$', '')
@@ -58,31 +61,30 @@ def sort_dataframe(df, col, ascending=True):
 
 def sort_name(df):
     '''Returns sorted dataframe value names.'''
-    return sort_dataframe(df, 'NameUpper', True).ix[:, [1, 3, 4, 5, 6]]
+    return sort_dataframe(df, 'NameUpper', True).iloc[:, [1, 3, 4, 5, 6]]
 
 def sort_marketcap(df):
     '''Returns sorted MarketCap values.'''
-    return sort_dataframe(df, 'MarketCap', False).ix[:, [1, 3]]
+    return sort_dataframe(df, 'MarketCap', False).iloc[:, [1, 3]]
 
 def sort_price(df):
     '''Returns sorted price values.'''
-    return sort_dataframe(df, 'Price', False).ix[:, [1, 4]]
-
+    return sort_dataframe(df, 'Price', False).iloc[:, [1, 4]]
 def sort_volume(df):
     '''Returns sorted volume values.'''
-    return sort_dataframe(df, 'VolumeDay', False).ix[:, [1, 6]]
+    return sort_dataframe(df, 'VolumeDay', False).iloc[:, [1, 6]]
 
 def sort_hour(df):
     '''Returns sorted hourly percentages.'''
-    return sort_dataframe(df, 'pctHour', False).ix[:, [1, 7]]
+    return sort_dataframe(df, 'pctHour', False).iloc[:, [1, 7]]
 
 def sort_day(df):
     '''Returns sorted daily percentages.'''
-    return sort_dataframe(df, 'pctDay', False).ix[:, [1, 8]]
+    return sort_dataframe(df, 'pctDay', False).iloc[:, [1, 8]]
 
 def sort_week(df):
     '''Returns sorted weekly percentages.'''
-    return sort_dataframe(df, 'pctWeek', False).ix[:, [1, 9]]
+    return sort_dataframe(df, 'pctWeek', False).iloc[:, [1, 9]]
 
 # Printing sorted dataframe in a tabulated format
 def print_tabulated(df):
