ee363e49477ce82124b5f5dfc4d5b5866fd88c5e
wildlyclassyprince <lihtumb@gmail.com>
2018-08-21 09:00:03 +0200

    Added top 10 script

---
+++ coinmarketcap/top_ten/.ipynb_checkpoints/coinmarketcap_top_10-checkpoint.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+'''
+This script downloads data from `CryptoCurrency Market Capitalizations`.
+It filters for Ethereum data with a Market Cap.
+Data is downloaded from https://coinmarketcap.com/assets/views/all/
+'''
+
