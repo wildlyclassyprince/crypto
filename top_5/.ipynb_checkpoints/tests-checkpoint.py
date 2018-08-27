2e2ce2b12c42f7e8e12fab85053ac8d1c2b5859b
wildlyclassyprince <lihtumb@gmail.com>
2018-08-27 14:54:24 +0200

    Edited tests.py

--- top_5/.ipynb_checkpoints/tests-checkpoint.py
+++ top_5/.ipynb_checkpoints/tests-checkpoint.py
@@ -19,6 +19,38 @@ import coinmarketcap_top_5 as top5
 
 URL = 'https://coinmarketcap.com/tokens/views/all'
 
+# Using Pandas to return the first table on the page
+df = pd.read_html(URL, attrs={'id': 'assets-all'})[0]
+
+# New column names:
+df.columns = ['#', 'Name', 'Platform', 'MarketCap', 'Price', 'CirculatingSupply',
+              'VolumeDay', 'pctHour', 'pctDay', 'pctWeek', 'NewCol']
+
+df = df.drop('NewCol', axis=1)
+
+# Cleaning numeric data:
+df['Name'] = df['Name'].apply(lambda x: x.upper())
+df['Price'] = df['Price'].str.replace('$', '')
+df['MarketCap'] = df['MarketCap'].str.replace('$', '')
+df['MarketCap'] = df['MarketCap'].str.replace(',', '')
+df['VolumeDay'] = df['VolumeDay'].str.replace('$', '')
+df['VolumeDay'] = df['VolumeDay'].str.replace(',', '')
+df['VolumeDay'] = df['VolumeDay'].str.replace('Low Vol', '0')
+df['pctHour'] = df['pctHour'].str.replace('%', '')
+df['pctDay'] = df['pctDay'].str.replace('%', '')
+df['pctWeek'] = df['pctWeek'].str.replace('%', '')
+
+# Filter for rows only containing Ethereum and a MarketCap value
+df = df.loc[(df['Platform'] == 'Ethereum') & (df['MarketCap'] != '?')]
+
+# Convert numeric columns to numeric type
+def coerce_df_columns_to_numeric(df, column_list):
+    '''Convert numeric columns to numeric type.'''
+    df[column_list] = df[column_list].apply(pd.to_numeric, errors='coerce')
+
+coerce_df_columns_to_numeric(df, ['MarketCap', 'Price', 'CirculatingSupply',
+                                  'VolumeDay', 'pctHour', 'pctDay', 'pctWeek'])
+
 # Value types
 def check_numeric_types():
     '''Checks if numeric values are of numeric type.'''
