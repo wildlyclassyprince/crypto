ba5714705669a431ee07db7bf51fe71ec1a104eb
wildlyclassyprince <lihtumb@gmail.com>
2018-08-27 09:53:29 +0200

    Edited comments

--- top_5/coinmarketcap_top_5.py
+++ top_5/coinmarketcap_top_5.py
@@ -30,7 +30,6 @@ df = pd.read_html(url, attrs={'id': 'assets-all'})[0]
 df.columns = ['#', 'Name', 'Platform', 'MarketCap', 'Price', 'CirculatingSupply',
               'VolumeDay', 'pctHour', 'pctDay', 'pctWeek', 'NewCol']
 
-# Dropping the new column
 df = df.drop('NewCol', axis=1)
 
 # Cleaning numeric data:
@@ -60,37 +59,30 @@ def sort_dataframe(df, col, ascending=True):
     '''Returns sorted dataframe values.'''
     return df.sort_values([col], ascending=ascending)
 
-# To sort names:
 def sort_name(df):
     '''Returns sorted dataframe value names.'''
     return sort_dataframe(df, 'Name', True).iloc[:, [1, 3, 5, 6]].head()
 
-# To sort marketcap values:
 def sort_marketcap(df):
     '''Returns sorted MarketCap values.'''
     return sort_dataframe(df, 'MarketCap', False).iloc[:, [1, 3]].head()
 
-# To sort the price:
 def sort_price(df):
     '''Returns sorted price values.'''
     return sort_dataframe(df, 'Price', False).iloc[:, [1, 4]].head()
 
-# To sort the volume:
 def sort_volume(df):
     '''Returns sorted volume values.'''
     return sort_dataframe(df, 'VolumeDay', False).iloc[:, [1, 6]].head()
 
-# To sort the hourly percentage:
 def sort_hour(df):
     '''Returns sorted hourly percentages.'''
     return sort_dataframe(df, 'pctHour', False).iloc[:, [1, 7]].head()
 
-# To sort the daily percentages:
 def sort_day(df):
     '''Returns sorted daily percentages.'''
     return sort_dataframe(df, 'pctDay', False).iloc[:, [1, 8]].head()
 
-# To sort the weekly percentages:
 def sort_week(df):
     '''Returns sorted weekly percentages.'''
     return sort_dataframe(df, 'pctWeek', False).iloc[:, [1, 9]].head()
@@ -100,7 +92,7 @@ def print_tabulated(df):
     '''Prints the sorted dataframe in a tabular format.'''
     print(tabulate.tabulate(df, headers='keys', showindex='false', numalign='right'))
     
-# Reporting
+# Putting it all together:
 def report():
     print('Title     : ' + 'CryptoAsset Market Capitalizations (Top 10)')
     print('          : ' + 'Ethereum with Market Cap')
