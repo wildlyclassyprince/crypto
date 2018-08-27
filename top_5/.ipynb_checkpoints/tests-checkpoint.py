18ec08647d6f08902e3e76848bf7cba6ef79775b
wildlyclassyprince <lihtumb@gmail.com>
2018-08-27 14:37:27 +0200

    Added tests.py

---
+++ top_5/.ipynb_checkpoints/tests-checkpoint.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+'''
+This script contains the tests for `coinmarketcap_top_5.py`.
+'''
+
+__author__="wildlyclassyprince"
+__license__="GNU"
+__version__="0.1.0"
+__maintainer__="wildlyclassyprince"
+__email__="lihtumb@gmail.com"
+__status__="Initial Tests"
+
